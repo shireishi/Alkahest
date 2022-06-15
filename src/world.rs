844bf1229fee29295b99707076265aad6a5332e9
shireishi <elarajade984@gmail.com>
2022-06-15 13:27:57 -0700

    Recreating the file structure.

--- src/world.rs
+++ src/world.rs
@@ -1,48 +1,87 @@
-use bevy::{
-    asset::Handle,
-    ecs::system::EntityCommands
-};
+use std::cell::{RefCell, RefMut};
 
-use super::{
-    Mesh,
-    default,
-    Vec3,
-    GameEntity,
-    EntityType
-};
-
-struct TextureMap {
-    texture: String,
-    color: Vec3,
-    height: f32
+trait ComponentVec {
+    fn as_any(&self) -> &dyn std::any::Any;
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
+    fn push_none(&mut self);
 }
 
-#[derive(Default)]
-pub struct Game {
-    pub world_mesh: Vec<Vec<f32>>,
+impl<T: 'static> ComponentVec for RefCell<Vec<Option<T>>> {
+    fn as_any(&self) -> &dyn std::any::Any {
+        self as &dyn std::any::Any
+    }
 
-    pub height: f32,
-    pub width: f32,
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self as &mut dyn std::any::Any
+    }
 
-    pub players: Vec<GameEntity>
+    fn push_none(&mut self) {
+        self.get_mut().push(None)
+    }
 }
-impl Game {
-    pub fn new(
-        h: f32,
-        w: f32
-    ) -> Game {
-        Game {
-            height: h,
-            width: w,
-            ..default()
+
+pub struct World {
+    entities_count: usize,
+    component_vecs: Vec<Box<dyn ComponentVec>> // the trait `ComponentVec` cannot be made into an object consider moving `add_comp` to another trait
+} impl World {
+    pub fn new() -> Self {
+        Self {
+            entities_count: 0,
+            component_vecs: Vec::new()
         }
     }
-    pub fn add_object(
+
+    pub fn new_entity(&mut self) -> usize {
+        let entity_id = self.entities_count;
+        for component_vec in self.component_vecs.iter_mut() {
+            component_vec.push_none();
+        }
+        self.entities_count += 1;
+        entity_id
+    }
+
+    pub fn add_comp<ComponentType: 'static>(
         &mut self,
-        _object: EntityCommands,
-        _pos_x: f32,
-        _pos_y: f32
+        entity: usize,
+        component: ComponentType,
     ) {
-        return;
+        // Search for any existing ComponentVecs that match the type of the component being added.
+        for component_vec in self.component_vecs.iter_mut() {
+            if let Some(component_vec) = component_vec
+                .as_any_mut()
+                .downcast_mut::<RefCell<Vec<Option<ComponentType>>>>()
+            {
+                component_vec.borrow_mut()[entity] = Some(component);
+                return;
+            }
+        }
+
+        // No matching component storage exists yet, so we have to make one.
+        let mut new_component_vec: Vec<Option<ComponentType>> =
+            Vec::with_capacity(self.entities_count);
+
+        // All existing entities don't have this component, so we give them `None`
+        for _ in 0..self.entities_count {
+            new_component_vec.push(None);
+        }
+
+        // Give this Entity the Component.
+        new_component_vec[entity] = Some(component);
+        self.component_vecs
+            .push(Box::new(RefCell::new(new_component_vec)));
+    }
+
+    pub fn borrow<ComponentType: 'static>(
+        &self,
+    ) -> Option<RefMut<Vec<Option<ComponentType>>>> {
+        for component_vec in self.component_vecs.iter() {
+            if let Some(component_vec) = component_vec
+                .as_any()
+                .downcast_ref::<RefCell<Vec<Option<ComponentType>>>>()
+            {
+                return Some(component_vec.borrow_mut());
+            }
+        }
+        None
     }
 }
